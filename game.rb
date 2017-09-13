7033adb2c84b94ef2acfab0fe89707c502896506
Ignacio Huerta <ignacio@ihuerta.net>
2017-09-13 15:21:13 +0200

    hello50

--- game.rb
+++ game.rb
@@ -3,7 +3,10 @@ require 'gosu'
 class GameWindow < Gosu::Window
   def initialize(width=800, height=600, fullscreen=false)
     super
+    self.caption = 'Hello Movement'
     @x = @y = 10
+    @draws = 0
+    @buttons_down = 0
   end
 
   def update
@@ -13,11 +16,31 @@ class GameWindow < Gosu::Window
     @y += 1 if button_down?(Gosu::KbDown)
   end
 
+  def button_down(id)
+    close if id == Gosu::KbEscape
+    @buttons_down += 1
+  end
+
+  def button_up(id)
+    @buttons_down -= 1
+  end
+
+  def needs_redraw?
+    @draws == 0 || @buttons_down > 0
+  end
+
   def draw
-    @message = Gosu::Image.from_text("x:#{@x}   y:#{@y}", 30)
+    @draws += 1
+    @message = Gosu::Image.from_text(
+      self, info, Gosu.default_font_name, 30)
     @message.draw(@x, @y, 0)
   end
 
+  private
+
+  def info
+    "[x:#{@x};y:#{@y};draws:#{@draws}]"
+  end
 end
 
 window = GameWindow.new
